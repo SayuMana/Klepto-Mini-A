e526252fe77190fa0fd8e3cab7f3957b10842b22
SayuMana <vierzasyahputra@gmail.com>
2026-02-11 08:35:29 +0700

    Character Movement

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TextCore.Text;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController characterController;
+    public float movementSpeed;
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveX, 0, moveZ);
+
+        characterController.Move(move * movementSpeed * Time.deltaTime);
+
+        if (moveX == 0 && moveZ == 0) return;
+        float heading = MathF.Atan2(moveX, moveZ);
+        transform.rotation = Quaternion.Euler(0, heading * Mathf.Rad2Deg, 0);
+    }
+
+
+}
