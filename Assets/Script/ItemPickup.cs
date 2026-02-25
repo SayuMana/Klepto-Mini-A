bd1b22a0914332d570283d1e4d4706b7c9fc15e6
SayuMana <vierzasyahputra@gmail.com>
2026-02-25 11:01:39 +0700

    Item Pickup Feature
    udah dijadiin prefabs

---
+++ Assets/Script/ItemPickup.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public GameObject itemToPickup;
+    private bool playerInRange = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
